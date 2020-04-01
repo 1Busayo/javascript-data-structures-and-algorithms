c4d80f61a077ada22a992af67392182e112b0935
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-04-01 10:40:50 +0100

    create linear search

---
+++ linearSearch.js
@@ -0,0 +1,10 @@
+// iterate through the array and find
+// time complexity O(n)
+function linearSearch(array, n) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == n) {
+      return true;
+    }
+  }
+  return false;
+}
