32ce333925caf71a53d5467f13c2cbf8686f4759
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-04-01 14:23:56 +0100

    create count sort

---
+++ countSort.js
@@ -0,0 +1,29 @@
+/**
+ * Sort an array,
+ * Time Complexity O(k+n)
+ * Space Complexity O(1)
+ * @param {number[]} array The array to sort
+ * @returns {number []} Returns the sorted array
+ * @example
+ * countSort([5, 1, 2, 3, 4]) // returns [1, 2, 3, 4, 5]
+ */
+
+function countSort(array) {
+  let hash = {},
+    countArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (!hash[array[i]]) {
+      hash[array[i]] = i;
+    } else {
+      hash[array[i]]++;
+    }
+  }
+  for (const key in hash) {
+    // for any number of _element, add t to array
+    for (var i = 0; i < hash[key]; i++) {
+      countArray.push(parseInt(key));
+    }
+  }
+  return countArray;
+}
