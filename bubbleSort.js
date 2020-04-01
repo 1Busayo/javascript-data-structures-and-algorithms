753bfd1c7bfab5b8a49b54bdffeee0b8255306db
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-04-01 12:00:30 +0100

    create bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,34 @@
+/**
+ * Swap two elements in an array
+ * Time Complexity O(1)
+ * @param {number[]} array The array to search
+ * @param {number} index1 The first index
+ * @param {number} index2 The second index
+ * @example
+ * binarySearch([1, 2, 3, 4, 5], 1, 4) // array = [1, 5, 3, 4, 2]
+ */
+function swap(array, index1, index2) {
+  let temp = array[index1];
+  array[index1] = array[index2];
+  array[index2] = temp;
+}
+
+/**
+ * Sort an array,
+ * Time Complexity O(n*2)
+ * Space Complexity O(1)
+ * @param {number[]} array The array to sort
+ * @returns {number[]} Returns the sorted array
+ * @example
+ * bubbleSort([5, 1, 2, 3, 4]) // returns [1, 2, 3, 4, 5]
+ */
+function bubbleSort(array) {
+  for (let i = 0, arrayLength = array.length; i < arrayLength; i++) {
+    for (let j = 0; j <= i; j++) {
+      if (array[i] < array[j]) {
+        swap(array, i, j);
+      }
+    }
+  }
+  return array;
+}
